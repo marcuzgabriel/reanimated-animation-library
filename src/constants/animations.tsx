b6ee53b76bd590eaf9d673b19a302f7692984146
Marcuz Larsen <marcuzlarsen@MacBook-Pro-tilhrende-Marcuz.local>
2021-05-04 14:50:29 +0200

    Fix: Reduce offset

--- src/constants/animations.tsx
+++ src/constants/animations.tsx
@@ -2,7 +2,7 @@ export const DEFAULT_SNAP_POINT_BOTTOM_RATIO = 0.8;
 export const DEFAULT_SNAP_POINT_TOP = 0;
 export const DEFAULT_SCROLL_SNAP_POINT_RATIO = 0.1;
 export const DEFAULT_SNAP_POINT_AUTO_SCROLL_TO_BOTTOM = 50;
-export const OFFSET_START_SNAP_TO_BOTTOM = 50;
+export const OFFSET_START_SNAP_TO_BOTTOM = 40;
 export const DEFAULT_TIMING_CONFIG = {
   damping: 15,
   mass: 1,
