358cbcdd598654215e184fcab7a72349c232f53b
Marcuz Gabriel Larsen <marcuzgabriel@gmail.com>
2021-05-26 15:58:04 +0200

    Latest

--- reanimated-animation-library.d.ts
+++ reanimated-animation-library.d.ts
@@ -1,7 +1,7 @@
 // Project: https://github.com/marcuzgabriel/reanimated-animation-library
 // Typescript Version: 3.7.4
 
-declare module '@mgl/reanimated-animation-library' {
+declare module '@marcuzgabriel/reanimated-animation-library' {
   import React from 'react';
   import type { BottomSheetConfiguration } from './src/types';
 
